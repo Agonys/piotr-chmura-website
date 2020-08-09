699407f8fbafc2a1ec0865b719bb5ddbef7531f8
Agonys <30295329+Agonys@users.noreply.github.com>
2020-08-09 21:33:10 +0200

    Updates

---
+++ precache-manifest.8b12ee2d5374de7ca76aaf3c3c7f11e2.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4247dfaab84bdaefa4bd2b0e20ba506",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fefea7bffcb90d384c8",
+    "url": "/static/js/2.6fea6707.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6fea6707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d284e455fe089927dc8",
+    "url": "/static/js/main.b7d90afe.chunk.js"
+  },
+  {
+    "revision": "7f92f8b3aae2a4a81f49",
+    "url": "/static/js/runtime-main.2f96981b.js"
+  },
+  {
+    "revision": "9561a683ff012735b0a8fc750f49fd27",
+    "url": "/static/media/Background_cover.9561a683.png"
+  },
+  {
+    "revision": "a0db1642041e53433212054690ef777a",
+    "url": "/static/media/Cleanliness.a0db1642.svg"
+  },
+  {
+    "revision": "1b6702430dfad9c07ab29acdbac02904",
+    "url": "/static/media/Efficiency.1b670243.svg"
+  },
+  {
+    "revision": "f92e1eddc67c1513aa42070e73ac1e72",
+    "url": "/static/media/Facebook.f92e1edd.svg"
+  },
+  {
+    "revision": "1a3eddc8ef65a20f01c5a5883ab90305",
+    "url": "/static/media/Github.1a3eddc8.svg"
+  },
+  {
+    "revision": "3f2c32c0c38833e1a7f6adcaa704815b",
+    "url": "/static/media/HeroBackground.3f2c32c0.png"
+  },
+  {
+    "revision": "319a1a5579095cbd238a216da2d85891",
+    "url": "/static/media/LinkedIn.319a1a55.svg"
+  },
+  {
+    "revision": "8bc5b4454174ff1df8491e7356a6765e",
+    "url": "/static/media/Logo_flat.8bc5b445.svg"
+  },
+  {
+    "revision": "c4938903192c013daf334288aa21cf11",
+    "url": "/static/media/Responsivness.c4938903.svg"
+  },
+  {
+    "revision": "c7a680b0372440884b9a680371e7e3da",
+    "url": "/static/media/atom.c7a680b0.svg"
+  },
+  {
+    "revision": "013ac52d07e794e4b3943529959aa1cf",
+    "url": "/static/media/bloodstone_calculator.013ac52d.jpg"
+  },
+  {
+    "revision": "bde252eebb62447b7f3422e5a8f870da",
+    "url": "/static/media/bloodstone_wiki.bde252ee.jpg"
+  },
+  {
+    "revision": "946049763a82e8df539a9a44b23837d8",
+    "url": "/static/media/css3.94604976.svg"
+  },
+  {
+    "revision": "79110be42e09d15b851710c03c32196e",
+    "url": "/static/media/deftoapartments.79110be4.jpg"
+  },
+  {
+    "revision": "92e3ddfead0ea9f6149ca18a7805a389",
+    "url": "/static/media/discord.92e3ddfe.jpg"
+  },
+  {
+    "revision": "805052423abf5e37804e11b9f7fff41e",
+    "url": "/static/media/figma.80505242.svg"
+  },
+  {
+    "revision": "a87da37b2f4d3fa7ba5b269a203b548e",
+    "url": "/static/media/git.a87da37b.svg"
+  },
+  {
+    "revision": "5159ea36523919e57ee1b46bde9988cd",
+    "url": "/static/media/gulp.5159ea36.svg"
+  },
+  {
+    "revision": "3209552376ba67b45ed44934924bbbc8",
+    "url": "/static/media/html5.32095523.svg"
+  },
+  {
+    "revision": "c2de3a1c95b50ffc343d5f964537fa94",
+    "url": "/static/media/jquery.c2de3a1c.svg"
+  },
+  {
+    "revision": "9dbc04a4e2297dbb8c0a3da801ce8dad",
+    "url": "/static/media/js.9dbc04a4.svg"
+  },
+  {
+    "revision": "3e5a1246ef7b260e0e9552714723d602",
+    "url": "/static/media/json.3e5a1246.svg"
+  },
+  {
+    "revision": "60c2bdb8b85b43b3c5178581456a1baa",
+    "url": "/static/media/mysql.60c2bdb8.svg"
+  },
+  {
+    "revision": "b337a2fb0b1289e6757e632dc766ff6b",
+    "url": "/static/media/nodejs.b337a2fb.svg"
+  },
+  {
+    "revision": "b48008ee660b24a4fbe2468be230beb1",
+    "url": "/static/media/npm.b48008ee.svg"
+  },
+  {
+    "revision": "b9cd6f1b184c704ecc96a08c5505daca",
+    "url": "/static/media/php.b9cd6f1b.svg"
+  },
+  {
+    "revision": "683c95f4a32adcd91a509c75fead791f",
+    "url": "/static/media/piotr_chmura.683c95f4.jpg"
+  },
+  {
+    "revision": "f0793d9f85eb3f0d1c702c14d236ba9e",
+    "url": "/static/media/react.f0793d9f.svg"
+  },
+  {
+    "revision": "4e3c2a4c4eaa023673cb82419f2a1134",
+    "url": "/static/media/sass.4e3c2a4c.svg"
+  },
+  {
+    "revision": "8d0e6e74d4f2668ea5cd8e65ef39fa0b",
+    "url": "/static/media/willa_anna.8d0e6e74.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/piotr-chmura-website/precache-manifest.e71f9ce8bc661e7b6809b6dd7b486b8e.js"
+  "/precache-manifest.8b12ee2d5374de7ca76aaf3c3c7f11e2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/piotr-chmura-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
